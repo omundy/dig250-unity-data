687780fcb67cc5499080cf9464207f7b9c66ec3c
Owen Mundy <omundy@gmail.com>
2025-10-24 10:24:50 -0400

    Updates spawning example

--- Assets/Object_Manager/Scripts/BallManager.cs
+++ Assets/Object_Manager/Scripts/BallManager.cs
@@ -5,68 +5,107 @@ using UnityEngine;
 /**
  *  Instantiates ball(s) and sets origin/destinations
  */
-public class BallManager : MonoBehaviour {
+public class BallManager : MonoBehaviour
+{
 
-    public BoxCollider2D [] spawnColliders;
+    public Transform[] spawnPoints;
+    public bool useSpawnPoints = true;
+    public BoxCollider2D[] spawnColliders;
     public int originIndex = 0;
     public int destIndex = 0;
     public GameObject ballPrefab;
-	public float spawnRate = 1.0f;
+    public float spawnRate = 1.0f;
 
-    void Start ()
+    void Start()
     {
         // start the loop
-        StartCoroutine (InitCreatorLoop (1.0f));
+        StartCoroutine(InitCreatorLoop(1.0f));
     }
 
-    IEnumerator InitCreatorLoop (float wait)
+    IEnumerator InitCreatorLoop(float wait)
     {
-        while (true) {
-			// change the spawnRate if you like (untested)
-        	//spawnRate = Random.Range(1.0f, 3.0f);
-			// wait = spawnRate;
-			// after a moment
-            yield return new WaitForSeconds (wait);
+        while (true)
+        {
+            // change the spawnRate if you like (untested)
+            //spawnRate = Random.Range(1.0f, 3.0f);
+            // wait = spawnRate;
+            // after a moment
+            yield return new WaitForSeconds(wait);
+
             // create a new ball
-            CreateNewBall ();
+            if (useSpawnPoints)
+            {
+                CreateNewBallFromPoints();
+            }
+            else
+            {
+                CreateNewBallFromBounds();
+            }
         }
     }
+    
+        void CreateNewBallFromPoints()
+    {
+        // update origin index
+        originIndex++;
+        // make sure it isn't > than array
+        if (originIndex >= spawnPoints.Length) originIndex = 0;
+        // set destination index (to opposite side)
+        if (originIndex < 2) destIndex = originIndex + 2; // 0,2 or 1,3
+        else destIndex = originIndex - 2; // 2,0 or 3,1
 
-    void CreateNewBall ()
+        // get a position that doesn't contain any other colliders
+        Vector3 spawnPosition = spawnPoints[originIndex].position;
+        // get the spawn rotation
+        Quaternion spawnRotation = new Quaternion();
+        // no random rotation
+        spawnRotation.eulerAngles = new Vector3(0f, 0f, 0f);
+        // instantiate prefab @ spawn position
+        GameObject obj = (GameObject)Instantiate(ballPrefab, spawnPosition, spawnRotation);
+        // reference to script (contains all the other references we need)
+        Ball ballScript = obj.GetComponent<Ball>();
+        // call Init() on script
+        ballScript.Init(spawnPosition, spawnPoints[destIndex].position);
+        // parent under Manager
+        obj.transform.parent = gameObject.transform;
+    }
+
+    void CreateNewBallFromBounds()
     {
         // update origin index
         originIndex++;
         // make sure it isn't > than array
         if (originIndex >= spawnColliders.Length) originIndex = 0;
-        // set destination index
+        // set destination index (to opposite side)
         if (originIndex < 2) destIndex = originIndex + 2; // 0,2 or 1,3
         else destIndex = originIndex - 2; // 2,0 or 3,1
 
         // get a position that doesn't contain any other colliders
-        Vector3 spawnPosition = RandomPointInBounds (spawnColliders [originIndex].bounds);
+        Vector3 spawnPosition = RandomPointInBounds(spawnColliders[originIndex].bounds);
         // get the spawn rotation
-        Quaternion spawnRotation = new Quaternion ();
+        Quaternion spawnRotation = new Quaternion();
         // no random rotation
-        spawnRotation.eulerAngles = new Vector3 (0f, 0f, 0f);
+        spawnRotation.eulerAngles = new Vector3(0f, 0f, 0f);
         // instantiate prefab @ spawn position
-        GameObject obj = (GameObject)Instantiate (ballPrefab, spawnPosition, spawnRotation);
+        GameObject obj = (GameObject)Instantiate(ballPrefab, spawnPosition, spawnRotation);
         // reference to script (contains all the other references we need)
-        Ball ballScript = obj.GetComponent<Ball> ();
+        Ball ballScript = obj.GetComponent<Ball>();
         // call Init() on script
-        ballScript.Init (spawnPosition, RandomPointInBounds (spawnColliders [destIndex].bounds));
+        ballScript.Init(spawnPosition, RandomPointInBounds(spawnColliders[destIndex].bounds));
         // parent under Manager
         obj.transform.parent = gameObject.transform;
     }
 
 
+
     /**
      *  Return random Vector3 position inside bounds
      */
-    public static Vector3 RandomPointInBounds (Bounds bounds)
+    public static Vector3 RandomPointInBounds(Bounds bounds)
     {
-        return new Vector3 (
-            Random.Range (bounds.min.x, bounds.max.x),
-            Random.Range (bounds.min.y, bounds.max.y),
+        return new Vector3(
+            Random.Range(bounds.min.x, bounds.max.x),
+            Random.Range(bounds.min.y, bounds.max.y),
             0
         );
     }
